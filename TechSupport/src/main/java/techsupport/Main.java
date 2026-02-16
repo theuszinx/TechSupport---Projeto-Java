db7a738b36faf33c59a1db0e5dc44020ee6cccb5
Resloko <duckedevel@gmail.com>
2026-02-16 19:34:41 -0300

    Testes básicos (OrdemServico e Tecnico)

--- TechSupport/src/main/java/techsupport/Main.java
+++ TechSupport/src/main/java/techsupport/Main.java
@@ -1,12 +1,24 @@
 package techsupport;
 
+import techsupport.enums.NivelTecnico;
 import techsupport.enums.Prioridade;
+
 import techsupport.model.OrdemServico;
+import techsupport.model.Tecnico;
 
 class Main{
     public static void main(String[] args){
         OrdemServico ordem = new OrdemServico("Commitar para o github", Prioridade.BAIXA, 20, 2);
+        Tecnico tecnico = new Tecnico("Bananildo", NivelTecnico.JUNIOR);
 
+        
         System.out.println(ordem.toString());
+
+
+        System.out.println(tecnico.toString());
+        tecnico.ocupar();
+        System.out.println(tecnico.estaDisponivel());
+        tecnico.liberar();
+        System.out.println(tecnico.estaDisponivel());
     }
 }
